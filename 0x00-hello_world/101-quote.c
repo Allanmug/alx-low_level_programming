43ec6e4a8068771006d3e78da16d60da9f695996
Allanmug <mugishaeng@gmail.com>
2023-02-18 06:57:30 -0800

    retry

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void) 
 { 
-char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, phrase, sizeof(phrase)); 
 return (1);
 }
