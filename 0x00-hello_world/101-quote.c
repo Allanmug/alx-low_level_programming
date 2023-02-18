115eb8fd43b6f28c54cf724de5bd7c16c9747ffa
Allanmug <mugishaeng@gmail.com>
2023-02-18 07:00:53 -0800

    retry

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main- a program that prits the reqired statements
  * Return: 1
@@ -7,5 +8,5 @@ int main(void)
 { 
 char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, phrase, sizeof(phrase)); 
-return (1);
+return (0);
 }
