181e4dba8a3c81b524b2ed9600cdb2f8154d18ef
Allanmug <mugishaeng@gmail.com>
2023-02-18 07:10:04 -0800

    retry

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  * main- a program that prits the reqired statements
  * Return: 1
  */
-int main(void) 
-{ 
+int main(void)
+{
 char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(1, phrase, sizeof(phrase)); 
+write(1,phrase,sizeof(phrase));
 return (1);
 }
