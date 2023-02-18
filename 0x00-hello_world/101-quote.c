2d37b196bf57ed9a664c06f17ba107a4825bec42
Allanmug <mugishaeng@gmail.com>
2023-02-18 06:51:22 -0800

    advanced 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- a program that prits the reqired statements
+ * Return: 1
+ */
+int main(void) 
+{ 
+char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+write(1, phrase, sizeof(phrase)); 
+return (1);
+}
