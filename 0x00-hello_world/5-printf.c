73bb5ebd6fb541a21d455528bf70509ae48bf007
Allanmug <mugishaeng@gmail.com>
2023-02-18 04:41:18 -0800

    printing using print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#iclude <stdio.h>
+/**
+ * main - program that uses print f to print
+ * Returns : value
+ */
+int main(void)
+{
+char word[]="with proper grammar, but the outcome is a piece of art,";
+printf("%l\n",word);
+return 0;
+}
