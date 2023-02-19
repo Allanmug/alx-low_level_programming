bdd0c68f1b82368d5a7c33cdbb56ae36b70729f5
Allanmug <mugishaeng@gmail.com>
2023-02-19 08:32:29 -0800

    retry

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - a program to print numbers from 0 to 9 with spaces
  * Return: 0
@@ -11,11 +12,12 @@ for (num = '0'; num <= '9'; num++)
 	putchar(num);
 	putchar(',');
 	putchar(' ');
-if(num == '8')
+if (num == '8')
 {
 	++num;
 	putchar(num);
 }
 }
+write(1, '\n', sizeof('\n'));
 return (0);
 }
