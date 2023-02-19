f055c220a4e430e0b0071a78a4e58bc4bb4d638d
Allanmug <mugishaeng@gmail.com>
2023-02-19 08:06:30 -0800

    almost

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program to print numbers from 0 to 9 with spaces
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+	putchar(num);
+	putchar(', ');
+if(num == '8')
+{
+	++num;
+	putchar(num);
+}
+}
+putchar('\n');
+return (0);
+}
