67fc47f80dae5b5e42c912101730f1a716455717
Allanmug <mugishaeng@gmail.com>
2023-02-19 09:08:51 -0800

    hope

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,12 @@ int num;
 for (num = '0'; num <= '9'; num++)
 {
 	putchar(num);
-	putchar(',');
-	putchar(' ');
 if (num == '8')
 {
-	++num;
-	putchar(num);
+	putchar(',');
+	putchar(' ');
 }
 }
+putchar('\n');
 return (0);
 }
