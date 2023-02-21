e7bc9f65f09914a81b760daa6f2bad054bfbcb37
Allanmug <mugishaeng@gmail.com>
2023-02-21 07:51:06 -0800

    the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - acode to print the alphabet
+ * Return: 0;
+ */
+int main(void)
+{
+	char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	_putchar(ch);
+}
+_putchar('\n')
+}
