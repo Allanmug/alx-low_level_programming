26404a420c4a0a613ee8bd1337f0f43eab4f3925
Allanmug <mugishaeng@gmail.com>
2023-02-22 08:37:28 -0800

    are we grater than zero

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign - a progrma to determine whether n is postive of negative
+ *@n: the character whos sign is to be determined
+ *
+ *Return: int or sign
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+	-putchar ('+');
+	return (1);
+}
+else if (n == 0)
+{
+	_putchar(0);
+	return (0);
+}
+else
+{
+	_putchar('-');
+	return (-1);
+}
+}
