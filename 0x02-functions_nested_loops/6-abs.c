aba74da71a2191fb1ca1620d119bd02a5026a870
Allanmug <mugishaeng@gmail.com>
2023-02-22 09:02:16 -0800

    about

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_abs - a progrma to print the absolute of a number
+ *
+ *Return: int or sign
+ */
+int _abs(int)
+{
+	int n;
+if (n > 0)
+{
+	_putchar (n);
+}
+else
+{
+	n = n * (-1);
+	_putchar(n);
+}
+}
