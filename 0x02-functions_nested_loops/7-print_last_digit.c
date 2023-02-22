78db58eb14c1727ae67be120de546209b99838d6
Allanmug <mugishaeng@gmail.com>
2023-02-22 10:05:54 -0800

    tryout

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,8 @@ int print_last_digit(int n)
 {
 if (n >= 0)
 {
-	n = n % 10;
+	n = n % '10';
 	_putchar(n);
-	return (n);
-}
-else
-{
-	n = n * (-1);
-	n = n % 10;
-	_putchar(n);
-	return (n);
+	return (n)
 }
 }
