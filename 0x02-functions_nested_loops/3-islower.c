697ef59f6a53e919071d24037b3a7544e576df47
Allanmug <mugishaeng@gmail.com>
2023-02-22 07:43:10 -0800

    betty is so strict

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- *_islower(int c) - a program to show if the char c is lower
- *Description: 'c is a parameter to be compared'
+ *_islower - a program to show if the char c is lower
+ *@c: the character for lower
+ *Description: 'c stands for any int passed in'
  *Return: 0 or 1
  */
 int _islower(int c)
+/*c :character c*/
 {
 if (c >= 'a' && c <= 'z')
 {
