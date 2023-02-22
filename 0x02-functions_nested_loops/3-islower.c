b9c20a7d8c91336c1d27aa1afa7be7ed9ea48613
Allanmug <mugishaeng@gmail.com>
2023-02-22 06:58:24 -0800

    moving foward

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,11 @@
 
 /**
  *_islower - a program to show if the char c is lower
- * c - parameter to be compared
+ *Descriptionn: 'c is a parameter to be compared'
  *Return: 0 or 1
  */
 int _islower(int c)
 {
-	int c;
-
 if (c >= 'a' && c <= 'z')
 {
 	return (1);
