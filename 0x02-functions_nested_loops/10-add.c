d280f2562d70e710ba444f695678d26e2e650084
Allanmug <mugishaeng@gmail.com>
2023-02-23 04:38:01 -0800

    renew

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * add - A program to give the sum of two numbers
+ * @a: input one
+ * @b: input two.
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
+
