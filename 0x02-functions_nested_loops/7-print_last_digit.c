bd2ac58ea469198f2816a5028a19e5cb7fc89347
Allanmug <mugishaeng@gmail.com>
2023-02-23 03:57:32 -0800

    one

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,14 @@
 int print_last_digit(int n)
 {
 	int num;
-if (n >= 0)
+
+	num = n %  10;
+
+if (num < 0)
 {
-	num = n % 10;
+	num = num * -1;
+}
 	_putchar(num + '0');
 	return (num);
 }
-else if
-{
-	n = n * (-1);
-	num = n % 10;
-	_putchar(num + '0');
-	return (num);
-}
+
