a6f9a94749cf10839d7d162d538965fb7a6d2ebd
Allanmug <mugishaeng@gmail.com>
2023-02-25 02:50:38 -0800

    moving one

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_islower - a program to show if the char c i uppercase
+ *@c: the character to be checked
+ *Description: 'c stands for any int passed in'
+ *Return: 0 or 1
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
