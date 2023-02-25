f9b57b0bd0c93ef43424ea5f25e55fab324199d8
Allanmug <mugishaeng@gmail.com>
2023-02-25 03:22:17 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isdigit - a program to show if the charactor c is a number
+ *@a: the number to be checked
+ *@b: the number to be checked
+ *Description: 'c stands for any int passed in'
+ *Return: int
+ */
+int mul(int a, int b);
+{
+	int mul ;
+	mul = a * b;
+	return (mul)
+}
